4511c01602152862db5ae96128f7b5ea9c3bb50a
吕春林 <2789327060@qq.com>
2020-09-23 22:24:49 +0800

    Initial commit

---
+++ src/main/java/com/lcl/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.lcl.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@RequestMapping("/user")
+@Controller
+public class UserController {
+
+    @RequestMapping("/login")
+    @ResponseBody
+    public String login(){
+        return "hello";
+    }
+}
